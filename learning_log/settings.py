91b62bc7ca59282e09efc0a3ac4bc95f6bd29da3
Nitin K. Mishra <42634998+nmishra1708@users.noreply.github.com>
2020-12-12 02:37:16 +0530

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -121,12 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles")
 LOGIN_URL = '/users/login/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
 
-STATIC_ROOT = "/var/www/example.com/static/"
+
+
+
